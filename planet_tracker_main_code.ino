b25fb31a9cd419bee566298bcd962f263e47d5a4
mgul999 <30603790+mgul999@users.noreply.github.com>
2017-12-31 12:39:55 -0500

    Update planet_tracker_main_code.ino

--- planet_tracker_main_code.ino
+++ planet_tracker_main_code.ino
@@ -1,9 +1,9 @@
- MPU9250 Basic Example Code
+ /*MPU9250 Basic Example Code
  by: Kris Winer
  date: April 1, 2014
  license: Beerware - Use this code however you'd like. If you 
  find it useful you can buy me a beer some time.
-
+*/
 
 
 
